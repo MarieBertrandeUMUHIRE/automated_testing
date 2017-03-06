e4f702114709b691ebfd7e13158f09355de75705
MarieBertrandeUMUHIRE <mariebertrande.umuhire@gmail.com>
2017-03-06 14:13:14 +0200

    added calculator

---
+++ calculator.rb
@@ -0,0 +1,67 @@
+require 'rspec'
+
+class Calculator
+  def add(number_one, number_two)
+    return number_one + number_two
+  end
+
+  def subtract(number_one, number_two)
+    return number_one - number_two
+  end
+
+  def multiply(number_one, number_two)
+    return number_one * number_two
+  end
+
+  def divide(dividend, divisor)
+    return dividend / divisor
+  end
+
+
+  def square(number)
+    return number * number
+  end
+
+  def power(number, exponent)
+    return number ** exponent
+  end
+end
+RSpec.describe Calculator do
+  describe '#substraction' do
+    it 'should substract two numbers' do
+      calculator = Calculator.new 
+      expect(calculator.subtract(4,2)).to eq(2)
+    end
+  end
+  describe '#addition' do
+    it 'should return the sum of two numbers' do
+     calculator = Calculator.new 
+     expect(calculator.add(4,1)).to eq(5)
+   end
+ end
+   describe '#multiplication' do
+    it 'should return the product of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(5,2)).to eq(10)
+    end
+  end
+
+  describe '#division' do
+    it 'should return the quotient of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(20,2)).to eq(10)
+    end
+  end
+  describe '#square' do
+    it 'should return the square of a number' do
+      calculator = Calculator.new
+      expect(calculator.square(5)).to eq(25)
+    end
+  end
+  describe '#power' do
+  it 'should return the a powered number' do
+      calculator = Calculator.new
+      expect(calculator.power(5,2)).to eq(25)
+    end
+  end
+end
